44c9fdb5259738eb64732d751015a49ec0fa9abd
nothing628 <yogyphang@gmail.com>
2022-12-25 17:01:44 +0700

    switch between production and dev assets

---
+++ start/view.ts
@@ -0,0 +1,42 @@
+/*
+|--------------------------------------------------------------------------
+| Preloaded File
+|--------------------------------------------------------------------------
+|
+| Any code written inside this file will be executed during the application
+| boot.
+|
+*/
+import View from "@ioc:Adonis/Core/View";
+import Env from "@ioc:Adonis/Core/Env";
+import Application from "@ioc:Adonis/Core/Application";
+import { readFileSync } from "node:fs";
+
+const getAssetScriptTag = (assetInfo) => {
+  const assetPath = "/dist/" + assetInfo.file;
+
+  return `<script src="${assetPath}"></script>`;
+};
+
+View.global("env", Env.get("NODE_ENV"));
+View.global("assetBuild", function (path: string) {
+  if (!path) return "";
+
+  try {
+    const manifestJsonPath = Application.publicPath("dist/manifest.json");
+    const manifestJsonContent = readFileSync(manifestJsonPath);
+    const manifestJson = JSON.parse(manifestJsonContent.toString());
+
+    if (path in manifestJson) {
+      const assetInfo = manifestJson[path];
+
+      return getAssetScriptTag(assetInfo);
+    }
+
+    return "<script></script>";
+  } catch {
+    //
+  }
+
+  return "";
+});
