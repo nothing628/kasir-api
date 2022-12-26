7df81053192148f22da7d796daadb751c0e65938
nothing628 <yogyphang@gmail.com>
2022-12-26 12:16:51 +0700

    add remixicon

--- resources/js/Components/TablePagination.vue
+++ resources/js/Components/TablePagination.vue
@@ -3,7 +3,7 @@
     <nav aria-label="Page navigation">
       <ul class="flex pl-0 justify-start">
         <li class="mx-[5px]">
-          <a class="block w-10 h-10 text-[#7e7e7e] bg-[#f2f3f4] rounded-[40px] text-center leading-[42px] font-bold transition-all hover:bg-[#3BB77E] hover:text-white" href="#"><i class="fi-rs-arrow-small-left"></i></a>
+          <a class="block w-10 h-10 text-[#7e7e7e] bg-[#f2f3f4] rounded-[40px] text-center leading-[42px] font-bold transition-all hover:bg-[#3BB77E] hover:text-white" href="#"><i class="ri-arrow-left-s-line"></i></a>
         </li>
         <li class="mx-[5px]"><a class="block w-10 h-10 text-[#7e7e7e] bg-[#f2f3f4] rounded-[40px] text-center leading-[42px] font-bold transition-all hover:bg-[#3BB77E] hover:text-white" href="#">1</a></li>
         <li class="mx-[5px]"><a class="block w-10 h-10 text-[#fff] bg-[#3BB77E] rounded-[40px] text-center leading-[42px] font-bold transition-all hover:bg-[#3BB77E] hover:text-white" href="#">2</a></li>
@@ -11,7 +11,7 @@
         <li class="mx-[5px]"><a class="block w-10 h-10 text-[#7e7e7e] bg-[#f2f3f4] rounded-[40px] text-center leading-[42px] font-bold transition-all hover:bg-[#3BB77E] hover:text-white" href="#">...</a></li>
         <li class="mx-[5px]"><a class="block w-10 h-10 text-[#7e7e7e] bg-[#f2f3f4] rounded-[40px] text-center leading-[42px] font-bold transition-all hover:bg-[#3BB77E] hover:text-white" href="#">6</a></li>
         <li class="mx-[5px]">
-          <a class="block w-10 h-10 text-[#7e7e7e] bg-[#f2f3f4] rounded-[40px] text-center leading-[42px] font-bold transition-all hover:bg-[#3BB77E] hover:text-white" href="#"><i class="fi-rs-arrow-small-right"></i></a>
+          <a class="block w-10 h-10 text-[#7e7e7e] bg-[#f2f3f4] rounded-[40px] text-center leading-[42px] font-bold transition-all hover:bg-[#3BB77E] hover:text-white" href="#"><i class="ri-arrow-right-s-line"></i></a>
         </li>
       </ul>
     </nav>
