6bf26fbdd0088a01e56ebaedf967617ec72fdf22
nothing628 <yogyphang@gmail.com>
2023-01-14 22:57:03 +0700

    add side menu

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-    <RouterView></RouterView>
+    <SideMenu></SideMenu>
+    <div class="ml-[300px] bg-[#f8f9fa] relative">
+      <section class="min-h-[calc(100vh-110px)] py-[30px] px-[3%] mx-auto bg-[#f8f9fa]">
+        <RouterView></RouterView>
+      </section>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import {RouterView} from 'vue-router'
+import { RouterView } from 'vue-router'
+import SideMenu from './Components/SideMenu.vue';
 </script>
---
+++ resources/js/Components/SideMenu.vue
@@ -0,0 +1,42 @@
+<template>
+  <aside class="w-full fixed block max-w-[300px] top-0 bottom-0 bg-white border-r border-r-[#eee]" id="offcanvas_aside">
+    <div class="">
+      <a href="/" class="">
+        <img src="/logo.svg" class="" alt="Nest Dashboard">
+      </a>
+      <div>
+        <button class=""><i class="text-muted material-icons md-menu_open"></i></button>
+      </div>
+    </div>
+    <nav>
+      <ul class="">
+        <li class="">
+          <a class="" href="/">
+            <i class="ri-home-4-line"></i>
+            <span class="">Dashboard</span>
+          </a>
+        </li>
+        <li class="">
+          <a class="" href="/">
+            <i class="ri-shopping-bag-3-line"></i>
+            <span class="">Products</span>
+          </a>
+        </li>
+        <li class="">
+          <a class="" href="/">
+            <i class="ri-shopping-cart-2-line"></i>
+            <span class="">Orders</span>
+          </a>
+        </li>
+        <li class="">
+          <a class="" href="/">
+            <i class="ri-money-dollar-circle-line"></i>
+            <span class="">Transactions</span>
+          </a>
+        </li>
+      </ul>
+      <br>
+      <br>
+    </nav>
+  </aside>
+</template>
